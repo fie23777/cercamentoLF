8745ae502ff3852eae8003341f63c3e27673180e
Fie2377 <fie2377@gmail.com>
2023-08-18 17:27:44 -0300

    finalizado os repositories

--- src/main/java/com/fie/cercamento/cercamentoLF/resource/resourcesoudados/soldadotres/IEsqTres.java
+++ src/main/java/com/fie/cercamento/cercamentoLF/resource/resourcesoudados/soldadotres/IEsqTres.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IEsqTres extends JpaRepository<FieEsq,EsqTresModel, Integer>{
+public interface IEsqTres extends JpaRepository<EsqTresModel, Integer>{
     
 }
